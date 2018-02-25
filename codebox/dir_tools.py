dc867ae57802b89108f3be23f7bb2bb44023edb8
Wilson Santos <wilson@codeminus.org>
2018-02-24 18:12:50 -0600

    add docstring

--- codebox/dir_tools.py
+++ codebox/dir_tools.py
@@ -2,6 +2,16 @@ import os
 
 
 def list_files(path, recursive=False):
+    """List of the absolute path of all files within a folder.
+    
+    Args:
+        path (str): The folder to be scanned for files.
+        recursive (bool, optional): Defaults to False. Whether or not to include all subdirectories.
+    
+    Yields:
+        str: The next file's absolute path within the folder
+    """
+
     if os.path.isdir(path):
         for dirpath, dirnames, filenames in os.walk(path):
             if dirnames and recursive:
@@ -9,8 +19,3 @@ def list_files(path, recursive=False):
                     list_files(os.path.join(dirpath, d))
             for f in filenames:
                 yield os.path.abspath(os.path.join(dirpath, f))
-
-
-if __name__ == '__main__':
-    for f in list_files('codebox'):
-        print(f)
