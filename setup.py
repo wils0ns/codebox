cc37b71d302195604041ff1c47d98e58eafbc7ab
Wilson Santos <wilson@codeminus.org>
2018-02-25 14:13:54 -0600

    fix package dependencies

--- setup.py
+++ setup.py
@@ -27,5 +27,7 @@ setup(
         'Programming Language :: Python :: 3'
     ],
     packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=[],
+    install_requires=[
+        'PyYAML'
+    ],
 )
