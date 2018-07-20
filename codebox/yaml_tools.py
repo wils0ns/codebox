0c07b56f0db78e27daff275d17a470dd0aa680b1
Wilson Santos <wilson@codeminus.org>
2018-07-19 22:24:40 -0500

    add option to filter yaml files to be loaded by a regex. add pylintrc

--- codebox/yaml_tools.py
+++ codebox/yaml_tools.py
@@ -1,33 +1,39 @@
 import os
 import yaml
+from re import match
 
 from codebox import dir_tools
 from codebox import dict_tools
 
-def load(path, recursive=False):
+def load(path, recursive=False, match_pattern=None):
     """Loads and parses a file or a folder containing yaml files.
     All existing dictorinaries will be deeply merged.
-    
+
     Args:
         path (str): File or directory path.
         recursive (bool, optional): Defaults to False. Whether or not to include subdirectoties.
-    
+        match_pattern (str, optional): A regular expression to be used to filter the files to be load
+            based on the file's full name.
+
     Returns:
         dict
     """
 
     files = []
-    if os.path.isdir(path):                
+    if os.path.isdir(path):
         files = dir_tools.list_files(path, recursive)
     else:
         files = [path]
 
     data = dict()
     for _file in files:
-        with open(_file) as f:            
+        if match_pattern and not match(match_pattern, _file):
+            continue
+        with open(_file) as f:
             data = dict_tools.merge(data, yaml.load(f.read()))
 
     return data
 
 if __name__ == '__main__':
+    print(load('examples/yml/', match_pattern='.*sls$'))
     print(load('examples/yml/'))
