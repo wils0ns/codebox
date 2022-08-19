0363bb6f6459b815f18b2c34f6bec765a7059bad
Wilson Santos <wilson@codeminus.org>
2022-08-19 11:44:29 +0200

    wip: update project files

--- setup.py
+++ setup.py
@@ -1,35 +1,4 @@
 """codebox package setup"""
-from os import path
-from codecs import open
-from setuptools import setup, find_packages
+from setuptools import setup
 
-THIS_FOLDER = path.abspath(path.dirname(__file__))
-
-# Get package version from .version file
-with open(path.join(THIS_FOLDER, '.version')) as f:
-    VERSION = f.read()
-
-# Get long description from README.rst file
-with open(path.join(THIS_FOLDER, 'README.rst'), encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
-
-setup(
-    name='codebox',
-    version=VERSION,
-    author='Wilson Santos',
-    author_email='wilson@codeminus.org',
-    url='https://gitlab.com/cathaldallan/codebox',
-    description='Python utility code collection',
-    long_description=LONG_DESCRIPTION,
-    license='MIT',
-    keywords='dict, yaml, dir',
-    classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
-    ],
-    packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=[
-        'PyYAML',
-        'jinja2'
-    ],
-)
+setup()
